c1803a4dfd8be2ce2aa849264b1d91187b3b76ea
tosaharuki <13haruki28@gmail.com>
2019-09-12 17:14:12 +0900

    commmit

--- src/views/About.vue
+++ src/views/About.vue
@@ -19,35 +19,20 @@
             <p>ホーム画面の授業を登録するボタンを押す。登録したい授業のパスワードを出席簿のオーナーに聞いて入力しよう。</p>
             <p>※この動作は初回のみ</p>
           </div>
-          <img
-            src="https://previews.dropbox.com/p/thumb/AAh_gDb5t6Hbj-WAu1iLwbgVUHp-FCzXI8F3b19eXjKrW3gIQq6z_YmaBgy0e8TkGn0qAWbNP1H_qZjSFGn-j3ZLYvDul7c_F7XJism2NmzqQBbHInHrboMoec38S8nTXEeJ4LbQO2-dEB_Cu7OQ9L6pNc9-bSxS84x76_V7TZOo74udQQgIVrOdoQqX9D0tY9pI0w4wM2EFLXMFwNDxC9tOR2DhAn5P_laa12tRmANERVxo7u-hjB0iYlRLE6wKVpw7nQ2GBbfx0fhnccwFKXDJAEffUMQ1Gv_7w952IUJmmuVneZSJwzm7-9xEo4p9hW9o8E6ePFo7oQFpYfKdlrO14qNEZp8KQMKob11AfwmGIQ/p.jpeg?fv_content=true&size_mode=5"
-            alt
-            width="300"
-            height="500"
-          />
+          <img src="../assets/03.jpeg" width="300" height="500" />
         </b-col>
         <b-col cols="12" md="6" lg="3" class="col-n bg-white">
           <div class="explain">
             <h3>2. Home画面の登録している授業から出席する授業をクリック</h3>
           </div>
-          <img
-            src="https://previews.dropbox.com/p/thumb/AAhLdBQy12M40EGaOWvGSHuGmBBqD-f8eVMYGIfIt9QmIERbRi9p-8uHu0SEXk4vBJN-jd_NM5L_r5PRb4czO7wtJ1Xp8dqnfUyyHrZgBDyUV-bDFmufQcBjw8qUwA-BSPTh2JtacoxXxk_FRJKPtIOASuAb5JMNcyFkUwO7iHjvhJaQaaj6Ca9a6OJDbjj_fleqj582Bg2NwGT944CjRvxje5JpnMoRIaUAsF-IRIFldcHZdvWGukn4-geNQ-0h61wFAVGGXm7TFiT0iCh5c_TqgJbMzcXBJuTZkrbLmtungymaEqnLzZe35OlOC5NCaZ3t95nNLQWTzS9i541eNelM/p.jpeg?fv_content=true&size_mode=5"
-            alt
-            width="300"
-            height="500"
-          />
+          <img src="../assets/02..jpeg" alt width="300" height="500" />
         </b-col>
         <b-col cols="12" md="6" lg="3" class="col-n bg-white">
           <div class="explain">
             <h3>3. ワンタイムパスワード入力して出席</h3>
             <p>オーナーからワンタイムパスワードを聞いて入力しよう！</p>
           </div>
-          <img
-            src="https://previews.dropbox.com/p/thumb/AAhWIZaypYBFkft2lWthUV6fB0S0PN_tJEXDIbv_wxvLbcnmO6bruCO8Fryl4i7Pdm34Pu86UyBsrf4lDwH3zVmhmmnxchg2bjLojKZCqoKAQZejwPTGTJiuwmsGqtzylYIimj6XmP_oWxmGmH92VW2Zo3oHVyxx7FG1wyvwLHAddMncg4D2nUZIMM0hSQyOEJ5RDB0aaM6WsM1T6-CsB82FELClqyqf32Lbc1eNP1lxrMJB-dd1ZX14wf8IFVaH2ttuSAcfNuFOcluU0Y_O4M9cw6xR6D90Lt7L3LDVCxQTvC9qwOT_4HH3aaH8PrHpIfS3HkNHPIoXQJxxBSgB27Iu/p.jpeg?fv_content=true&size_mode=5"
-            alt
-            width="300"
-            height="500"
-          />
+          <img src="../assets/01.jpeg" alt width="300" height="500" />
         </b-col>
         <b-col cols="12" md="6" lg="3" class="col-n bg-white">
           <p>次回からは出席する授業をクリックしてワンタイムパスワードを入力するだけ！</p>
@@ -55,39 +40,31 @@
         <b-col cols="12" md="12" class="col bg-blue-dark">
           <h2 class="color-white">出席簿を作成して出欠を取るには？</h2>
         </b-col>
-        <b-col cols="12" md="6" lg="3" class="col bg-white">
+        <b-col cols="12" md="6" lg="3" class="col-p bg-white">
           <div class="explain">
             <h3>1.出席簿を作成しよう</h3>
-            <p>ホーム画面の出席簿を作成するボタンを押す。タイトルとパスワードを入力しよう。
-            入力したパスワードを生徒に伝えて出席簿に登録してもらいましょう。
+            <p>
+              ホーム画面の出席簿を作成するボタンを押す。タイトルとパスワードを入力しよう。
+              入力したパスワードを生徒に伝えて出席簿に登録してもらいましょう。
             </p>
           </div>
-          <img
-            src="https://previews.dropbox.com/p/thumb/AAjTUwgn82HtFnqGa8i1obTZ9mXh1sPwP0KGEZ7GAhr1vfcVjJuN4YzIbu6Pag0SMfGDq23yg9uHmPYrTrHUZmBMf9MHaIXKT5ou6x62C4GghWFzYWbF-Fj3-pizx0vhweai-5HsHmIUjPY0j0iwS8ialErTI8M-9No16Xjxp_-i3pZTb98u_6nYfk4cyjy8QPuXbl2f3khnCGliqJ983F_8UnpLioKZFxTs0cFWVEOo8MRdLgtjY7QvNzIwtVB2lEw4NXKgkbRbOz1-olwptX8Owj1GSQ8EfYZ60A1xSH8t6bbHmxwe1n-5x9RYe_djDXftNxOKGOtVEGz94DgP1txC/p.jpeg?fv_content=true&size_mode=5"
-            width="300"
-            height="500"
-          />
+          <img src="../assets/06.jpeg" width="300" height="500" />
         </b-col>
-        <b-col cols="12" md="6" lg="3" class="col-n bg-white">
+        <b-col cols="12" md="6" lg="3" class="col-p bg-white">
           <div class="explain">
             <h3>2. Home画面のあなたの出席簿から出欠を取りたい授業をクリック</h3>
           </div>
-          <img
-            src="https://previews.dropbox.com/p/thumb/AAh4VeRuC5JswEl0Sfm-qhdjZfeAN_nvsII6DoQt0X7_lAjwRBGHtFC_U82R1bNlnyalTV-KS8KxY9GbD9d6xG19G9ottyWqbUw72CDc4V4_i_XrQUUMRTwec9Zg6d2XLO2R_bmemwtCfJ8l4BuH1BOavBGfohzLe1QVQtTbj_JYSQ11mcDSeAbFvCuI1PeXBpL2qdWDBaO2EVvdgqPPEv7__Y-ZNW7rkkYq_CMKEtcho-RwdPwYHjg1rW8ZlwyVIv8Kf7qrF2o_XLBj8c-sZmE0wtsFSMi4YVNyyl1sWs0OB5bgxQeG_xJxrzg-J9CiaFZAWdZ-roVET6p2oI5K9Ccm/p.jpeg?size=1280x960&size_mode=3"
-            width="300"
-            height="500"
-          />
+          <img src="../assets/05.jpeg" width="300" height="500" />
         </b-col>
-        <b-col cols="12" md="6" lg="3" class="col-n bg-white">
+        <b-col cols="12" md="6" lg="3" class="col-p bg-white">
           <div class="explain">
-            <h3>3. ワンタイムパスワード入力して出欠を取ります。出席簿に登録している生徒に伝えて出席してもらいましょう。</h3>
-            <p>出欠を取ったデータが出席簿に記録されます。</p>
+            <h3>3. ワンタイムパスワード入力して出欠を取ります。</h3>
+            <p>パスワードを出席簿に登録している生徒に伝えて出席してもらいましょう。出欠を取ったデータが出席簿に記録されます。</p>
           </div>
-          <img
-            src="https://previews.dropbox.com/p/thumb/AAhcSq31m1waoGf3Nwebh5bJKyF8Khb4opylqfGcX6PslMY-u9-CL9txxR4R3zAjLgNwl2S0VapCb08XNJusnTEb044X0kwQWNnXwrqEZaeAUs3KZlP7-9x3id4OIUlfX584xkMfYzmiVsCAhzMcMlF81rEGMYIr48Y-z0j_OO-To1g-Z77L7r86yIVNTzrDnECKp17rAj46yjdt6jwoLr5Fcx1KjrIL5bLNcIthlNeno_nEvN24YXCYAbNLLd5uE4Zo8geJkpDPVttwxT-hahirQKMwst2OXim_kFK12k3Xc0Ql5CpHC-6Q55duYDXde0s3FyW8wMIhte1tdG7H_qW8/p.jpeg?size=1280x960&size_mode=3"
-            width="300"
-            height="500"
-          />
+          <img src="../assets/04.jpeg" width="300" height="500" />
+        </b-col>
+        <b-col cols="12" md="6" lg="3" class="col bg-white">
+          <p>パスワードの有効時間は15分間です</p>
         </b-col>
       </b-row>
     </b-container-fluid>
@@ -126,6 +103,16 @@ export default {
   flex-flow: column nowrap;
   align-items: center;
   min-height: 30vh;
+  width: 100vw;
+  padding: 20px;
+}
+.col-p {
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  min-height: 35vh;
   width: 100vw;
   padding: 20px;
 }
